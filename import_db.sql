c7df9a398a784510d4dea600b14cbf189846cd58
Cornell Bethea <betheacornellb@gmail.com>
2022-12-07 12:03:38 -0500

    likes

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY
     fname TEXT NOT NULL
@@ -6,21 +8,39 @@ CREATE TABLE users (
 
 CREATE TABLE quetsions (
     id INTEGER PRIMARY KEY
-    body TEXT
-    title TEXT
-    author_id INTEGER
+    body TEXT NOT NULL
+    title TEXT NOT NULL
+    author_id INTEGER NOT NULL
     FOREIGN KEY (author_id) REFERENCES user(id)
 );
 
 CREATE TABLE quetsions_follows (
-    id INTEGER
-    users_id INTEGER
-    quetsions_id INTEGER
+    id INTEGER NOT NULL
+    users_id INTEGER NOT NULL
+    quetsions_id INTEGER NOT NULL
+    FOREIGN KEY (users_id) REFERENCES user(id)
+    FOREIGN KEY (quetsions_id) REFERENCES quetsions(id)
 );
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY 
-    
+    parent_reply_id INTEGER 
+    body TEXT NOT NULL
+    quetsions_id INTEGER
+    users_id INTEGER
+    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (questions_id) REFERENCES quetsions(id)
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY (id) REFERENCES quetsions_follows(id)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER
+    user_like INTEGER
+    user_id INTEGER
+    quetsions_id INTEGER
+    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (questions_id) REFERENCES quetsions(id)
 )
 
 
